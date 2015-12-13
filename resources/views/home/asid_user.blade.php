e224c83358ee32b41bbae5875e637dd833c6e54b
motecshine <shinemotec@gmail.com>
2015-12-14 02:45:00 +0800

    修正登陆 以及左边导航栏

---
+++ resources/views/home/asid_user.blade.php
@@ -0,0 +1,22 @@
+<!-- user -->
+<div class="clearfix hidden-xs text-center hide" id="aside-user">
+    <div class="dropdown wrapper">
+        <a href="app.page.profile">
+                <span class="thumb-lg w-auto-folded avatar m-t-sm">
+                  <img src="img/a0.jpg" class="img-full" alt="...">
+                </span>
+        </a>
+        <a href="#" data-toggle="dropdown" class="dropdown-toggle hidden-folded">
+                <span class="clear">
+                  <span class="block m-t-sm">
+                    <strong class="font-bold text-lt">John.Smith</strong>
+                    <b class="caret"></b>
+                  </span>
+                  <span class="text-muted text-xs block">Art Director</span>
+                </span>
+        </a>
+
+    </div>
+    <div class="line dk hidden-folded"></div>
+</div>
+<!-- / user -->
